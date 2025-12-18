68218f0b719310a369ba6f6de38a78bd4f2eee39
unknown <DienAkbar25>
2025-12-18 11:23:47 +0700

    misc update

--- backend/src/routes/film/film.genre.routes.js
+++ backend/src/routes/film/film.genre.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const genreController = require("../../controllers/film/film.genre.controller");
 
-// GET /api/films/by-genre?genre=Drama&page=1
+//api/films/by-genre?genre=Drama&page=1
 router.get("/by-genre", genreController.getFilmsByGenre);
 
 module.exports = router;
