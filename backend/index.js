6c4c1374e780242b7653c06e6c87c8d5f5648b62
unknown <DienAkbar25>
2025-12-18 09:20:45 +0700

    before update

--- backend/index.js
+++ backend/index.js
@@ -58,10 +58,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-/* =========================
-   SERVER
-========================= */
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
